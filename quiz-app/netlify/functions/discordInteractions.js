df0c80ecba6172cd1d548a9c7b549bcc753dff3e
Natti94 <natnael.berhane.sv@gmail.com>
2025-10-27 21:12:51 +0100

    update: discord intercations - access key, for exams. User can now generate a one time use KEY from discord channel 'Quiz-Access' to unlock the form

--- quiz-app/netlify/functions/discordInteractions.js
+++ quiz-app/netlify/functions/discordInteractions.js
@@ -91,43 +91,7 @@ export const handler = async (event) => {
 
       const content = `Din admin-nyckel (giltig i ${ttlMinutes} min): ${code}`;
 
-      const response = {
-        type: 4,
-        data: { content, flags: 64 },
-      };
-
-      if (botToken && data.member?.user?.id) {
-        try {
-          const userId = data.member.user.id;
-          const dmRes = await fetch(
-            "https://discord.com/api/v10/users/@me/channels",
-            {
-              method: "POST",
-              headers: {
-                Authorization: `Bot ${botToken}`,
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({ recipient_id: userId }),
-            },
-          );
-          const dm = await dmRes.json();
-          if (dm?.id) {
-            await fetch(
-              `https://discord.com/api/v10/channels/${dm.id}/messages`,
-              {
-                method: "POST",
-                headers: {
-                  Authorization: `Bot ${botToken}`,
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ content }),
-              },
-            );
-          }
-        } catch {}
-      }
-
-      return jsonResponse(response);
+      return jsonResponse({ type: 4, data: { content, flags: 64 } });
     }
   }
 
