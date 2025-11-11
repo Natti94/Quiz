7c1d55f02edfa52704bb3d990ea140a7c3ce77ff
Natti94 <natnael.berhane.sv@gmail.com>
2025-11-11 20:32:23 +0100

    fix: attempting to solve issues with camelcasing on deployment

--- quiz-frontend/src/components/statistics/Statistics.jsx
+++ quiz-frontend/src/components/statistics/Statistics.jsx
@@ -2,7 +2,7 @@ import Leaderboard from "./statistics-wrapper/leaderboard/Leaderboard";
 import Points from "./statistics-wrapper/leaderboard/leaderboard-wrapper/Points";
 import Speed from "./statistics-wrapper/leaderboard/leaderboard-wrapper/Speed";
 import Excellence from "./statistics-wrapper/leaderboard/leaderboard-wrapper/Excellence";
-import "./Statistics.css";
+import "./statistics.css";
 
 const Statistics = ({ activeCategory }) => {
   switch (activeCategory) {
