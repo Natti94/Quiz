9b231d7b5fed172a532eb720b50542dce399dc00
Natti94 <natnael.berhane.sv@gmail.com>
2025-11-17 14:33:07 +0100

    chore(release): local plugin for updating subpackage versions

---
+++ scripts/semantic-release-update-subpackages.mjs
@@ -0,0 +1,17 @@
+import { execFileSync } from 'node:child_process';
+
+export default {
+  prepare: async (_, context) => {
+    const { nextRelease, logger } = context;
+    logger.log(`Running local plugin: updating subpackage versions to ${nextRelease.version}`);
+    try {
+      execFileSync(process.execPath, ['scripts/update-subpackage-versions.mjs', nextRelease.version], {
+        stdio: 'inherit',
+      });
+    } catch (err) {
+      logger.error('Failed to update subpackage versions:');
+      logger.error(err);
+      throw err;
+    }
+  },
+};
